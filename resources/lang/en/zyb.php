02104cdd0a8007eb7db74bd4dfe80c35d35b2b48
cristian bono <cristianbono87@gmail.com>
2020-08-23 12:23:22 -0300

    Creacion de pagina web responsive de ingenieria y gestion

---
+++ resources/lang/en/zyb.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web site Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'get_started' => 'COMENZAR',
+    'our_services' => 'NUESTROS SERVICIOS',
+    'name'=>'ZyB INGENIERIA',
+    'service_1'=>'Gestión e ingeniería de procesos',
+    'service_2'=>'Servicios de calidad',
+    'service_3'=>'Mejora continua, brindar valor agregado',
+
+];
