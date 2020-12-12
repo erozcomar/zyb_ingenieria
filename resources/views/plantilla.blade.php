593d2581673a8376c889278482eeda500dbb17d3
cristian bono <cristianbono87@gmail.com>
2020-12-12 19:51:06 -0300

    cambios varios

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,39 +1,72 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <meta charset="utf-8">
+  <title>ZYB | INGENIERIA</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
+  <meta property="og:title" content="">
+  <meta property="og:image" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
 
-    <title>plantilla</title>
-  </head>
+  <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="">
+  <meta name="twitter:title" content="">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:image" content="">
+
+  <!-- Place your favicon.ico and apple-touch-icon.png in the template root directory -->
+  <link href="/template/img/zyb_logo2.png" rel="shortcut icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="/template/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="/template/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="/template/lib/animate-css/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="/template/css/style.css" rel="stylesheet">
+
+</head>
   <body>
 
 <!-- barra de navegacion -->
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="{{ route('principal') }}">ZyB</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" href="{{ route('bienvenidos') }}">Bienvenidos <span class="sr-only">(current)</span></a>
-      <a class="nav-link" href="{{ route('admin') }}">Administrador</a>
-      <a class="nav-link" href="{{ route('nueva_seccion') }}">Nueva Seccion</a>
-      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </div>
-  </div>
-</nav>
+<header id="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+        <a href="#hero"><img src="/template/img/zyb_logo5.png" alt="" title="" /></img></a>
+      </div>
 
-  <!-- <div class='container'><h1>Cabecera</h1>
-  <a href="{{ route('bienvenidos') }}" class='btn btn-primary'>Bienvenidos</a>
-  <a href="{{ route('admin') }}" class='btn btn-primary'>Administrador</a>
-  <a href="{{ route('nueva_seccion') }}" class='btn btn-primary'>Nueva seccion</a>
-</div> -->
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">{{trans('zyb.home')}}</a></li>
+          <li><a class="nav-link active" href="{{ route('bienvenidos') }}">Bienvenidos <span class="sr-only">(current)</span></a></li>
+          <li><a class="nav-link" href="{{ route('admin') }}">Administrador</a></li>
+          <li><a class="nav-link" href="{{ route('nueva_seccion') }}">Nueva Seccion</a></li>
+          <li><a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+ <!--==========================
+  Services Section
+  ============================-->
+  <section id="services">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        
 
 <!-- aca se enuentra nuestro contenido de la pagina -->
 
@@ -43,16 +76,54 @@
 
 <!-- ----------------------------------------------- -->
 
+      </div>
+    </div>
+  </section>
+
+ 
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">Footer
     </div>
   </div>
 </nav>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+ <!--==========================
+  Footer
+============================-->
+
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright">
+            <strong>Córdoba, Argentina</strong>
+          </div>
+          <div class="credits">
+        
+            <a href="https://linkedin.com/in/cristian-bono-11636b103/">Develop By Cristian Bono</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- #footer
+
+  Required JavaScript Libraries -->
+  <script src="/template/lib/jquery/jquery.min.js"></script>
+  <script src="/template/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="/template/lib/superfish/hoverIntent.js"></script>
+  <script src="/template/lib/superfish/superfish.min.js"></script>
+  <script src="/template/lib/morphext/morphext.min.js"></script>
+  <script src="/template/lib/wow/wow.min.js"></script>
+  <script src="/template/lib/stickyjs/sticky.js"></script>
+  <script src="/template/lib/easing/easing.js"></script>
+
+  <!-- Template Specisifc Custom Javascript File -->
+  <script src="/template/js/custom.js"></script>
+
+  <script src="/template/contactform/contactform.js"></script>
   </body>
 </html>
---
+++ resources/views/servicio1.blade.php
@@ -0,0 +1,11 @@
+@extends('plantilla')
+
+@section('seccion')
+
+<div class="col-md-12">
+    <h3 class="section-title">{{trans('zyb.servicio1')}}</h3>
+    <div class="section-title-divider"></div>
+    <p class="section-description"></p>
+</div>
+
+@endsection
