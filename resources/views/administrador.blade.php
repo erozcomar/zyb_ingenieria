78dba735f32f9e147e569922b82698b027649b09
cristian bono <cristianbono87@gmail.com>
2021-01-03 21:30:33 -0300

    se agrega boton linkedin y se centran textos

--- resources/views/administrador.blade.php
+++ resources/views/administrador.blade.php
@@ -2,6 +2,11 @@
 
 @section('seccion')
 
-<h1>Consultas</h1>
+<h1>Administrador</h1>
+
+<!-- Contador de visitas -->
+<center><a href="http://www.websmultimedia.com/contador-de-visitas-gratis" title="Contador De Visitas Gratis">
+<img style="border: 0px solid; display: inline;" alt="contador de visitas" src="http://www.websmultimedia.com/contador-de-visitas.php?id=289093"></a><br><a href='http://www.websmultimedia.com/contador-de-visitas-gratis'>Contador de visitas</a><br><a href='http://www.websmultimedia.com'>diseño web economico</a></center>
+<!-- Fin Contador de visitas -->
 
 @endsection
